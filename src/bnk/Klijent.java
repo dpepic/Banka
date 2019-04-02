90719fb3d3532609cd7dd751c8550ac480f354ee
Dragan <rechiefy@gmail.com>
2019-04-02 10:24:16 +0200

    Neki commit

--- src/bnk/Klijent.java
+++ src/bnk/Klijent.java
@@ -3,13 +3,13 @@ package bnk;
 import java.util.Vector;
 
 public class Klijent 
-{
+{	
 	Vector<Racun> racuni = new Vector<Racun>();
 	String id;
 	
 	public Klijent(String id, String rac)
 	{
 		this.id = id;
-		//nasRacun = new Racun(rac);
+		racuni.add(new Racun(rac));
 	}
 }
